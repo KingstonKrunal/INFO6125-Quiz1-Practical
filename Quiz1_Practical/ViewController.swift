1595e40d9c8432a63c4f920b4f6cf51ef3d3dbfb
KingstonKrunal <krunalshah661@gmail.com>
2022-02-09 18:43:17 -0500

    Initial Commit

---
+++ Quiz1_Practical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz1_Practical
+//
+//  Created by Krunal Shah on 2022-02-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
